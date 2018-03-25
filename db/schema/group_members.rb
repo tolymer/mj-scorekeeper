2d1441da6e6df50e85258ee06df240433af9cc6f
Kazuhito Hokamura <k.hokamura@gmail.com>
2018-03-25 22:34:22 +0900

    modeling

---
+++ db/schema/group_members.rb
@@ -0,0 +1,7 @@
+create_table 'group_members', force: :cascade do |t|
+  t.bigint   'user_id',    null: false
+  t.bigint   'group_id',   null: false
+  t.datetime 'created_at', null: false
+end
+
+add_index 'group_members', ['user_id', 'group_id'], name: 'idx_user_id_group_id', unique: true, using: :btree
---
+++ db/schema/users.rb
@@ -0,0 +1,6 @@
+create_table 'users', force: :cascade do |t|
+  t.string   'name',       null: false
+  t.datetime 'created_at', null: false
+end
+
+add_index 'users', 'name', name: 'idx_name', unique: true, using: :btree
