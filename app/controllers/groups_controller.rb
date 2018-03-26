939dccb8e954e0e6408f9547b979940cf49af37c
Kazuhito Hokamura <k.hokamura@gmail.com>
2018-03-26 22:30:00 +0900

    groups and group_members API

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,11 @@
+class GroupsController < ApplicationController
+  def show
+    group = Group.find(params[:id])
+    render json: group
+  end
+
+  def create
+    group = Group.create!(name: params[:name], members: [current_user])
+    render json: group, status: 201
+  end
+end
---
+++ spec/requests/group_members_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+describe 'group members API' do
+  let(:current_user) { FactoryBot.create(:user) }
+  let(:headers) {
+    token = Knock::AuthToken.new(payload: { sub: current_user.id }).token
+    { Authorization: "Bearer #{token}" }
+  }
+
+  subject(:status) { response.status }
+  subject(:body) { JSON.parse(response.body) }
+
+  describe 'GET /groups/:group_id/members' do
+    let(:group) { FactoryBot.create(:group) }
+    let(:user1) { FactoryBot.create(:user) }
+    let(:user2) { FactoryBot.create(:user) }
+
+    before do
+      group.members << user1
+      group.members << user2
+      get "/groups/#{group.id}/members", headers: headers
+    end
+
+    specify do
+      expect(status).to be 200
+      expect(body).to contain_exactly(
+        { 'id' => user1.id, 'name' => user1.name },
+        { 'id' => user2.id, 'name' => user2.name }
+      )
+    end
+  end
+
+  describe 'POST /groups/:group_id/members' do
+    let(:group) { FactoryBot.create(:group) }
+    let(:user) { FactoryBot.create(:user) }
+    before { post "/groups/#{group.id}/members", params: { user_id: user.id }, headers: headers }
+
+    specify do
+      expect(status).to be 201
+      expect(body).to eq({ 'id' => user.id, 'name' => user.name })
+      expect(group.members.size).to eq 1
+      expect(group.members.take).to eq user
+    end
+  end
+end
