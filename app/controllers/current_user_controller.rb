2c79cbacf385f8d88e4de1d2a2f24fd80e5f4b11
Kazuhito Hokamura <k.hokamura@gmail.com>
2018-03-31 19:48:56 +0900

    GET /current_user/groups

--- app/controllers/current_user_controller.rb
+++
@@ -1,5 +0,0 @@
-class CurrentUserController < ApplicationController
-  def index
-    render json: current_user
-  end
-end
--- spec/requests/current_user_spec.rb
+++ spec/requests/current_user_spec.rb
@@ -10,12 +10,29 @@ describe 'current_user API' do
   subject(:status) { response.status }
   subject(:body) { JSON.parse(response.body) }
 
-  describe 'GET /current_users' do
+  describe 'GET /current_user' do
     before { get '/current_user', headers: headers }
 
     specify do
       expect(status).to be 200
       expect(body).to eq({ 'id' => current_user.id, 'name' => current_user.name })
+    end
+  end
+
+  describe 'GET /current_user/groups' do
+    let(:group1) { FactoryBot.create(:group) }
+    let(:group2) { FactoryBot.create(:group) }
+    let(:another_user) {FactoryBot.create(:user) }
+
+    before do
+      group1.members << current_user
+      group2.members << another_user
+      get '/current_user/groups', headers: headers
+    end
+
+    specify do
+      expect(status).to be 200
+      expect(body).to eq([{ 'id' => group1.id, 'name' => group1.name }])
     end
   end
 end
