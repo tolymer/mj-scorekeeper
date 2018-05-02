064caed05b56d3099f7519411a7e1bc5569fc1f1
1000ch <shogo.sensui@gmail.com>
2018-05-02 14:55:05 +0900

    Fix web server URL

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,8 @@
 oauth_config = Rails.application.config_for(:omniauth).deep_symbolize_keys
 google_config = oauth_config[:google]
 
+OmniAuth.config.full_host = Rails.env.production? ? 'https://app.tolymer.com' : 'http://localhost:7700'
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, google_config[:client_id], google_config[:secret], scope: 'profile', name: 'google'
 end
