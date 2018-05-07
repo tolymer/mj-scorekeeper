eb504dc997aee8de33ac8dac3086fb6c39f29e73
Kazuhito Hokamura <k.hokamura@gmail.com>
2018-05-08 00:03:09 +0900

    Drop users.password_digest

--- db/schema/users.rb
+++ db/schema/users.rb
@@ -1,7 +1,4 @@
 create_table 'users', force: :cascade do |t|
-  t.string   'name',            null: false
-  t.string   'password_digest', null: false
-  t.datetime 'created_at',      null: false
+  t.string   'name',       null: false
+  t.datetime 'created_at', null: false
 end
-
-add_index 'users', 'name', name: 'idx_name', unique: true, using: :btree
