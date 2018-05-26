64bb1608282f8776fa5a7f2157d5f2aeded0a41f
1000ch <shogo.sensui@gmail.com>
2018-05-26 23:00:48 +0900

    Accept PATCH request to users/:id

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,17 @@
 class UsersController < ApplicationController
-  skip_before_action :authenticate_user, only: [:show, :create]
+  skip_before_action :authenticate_user, only: [:show, :update]
 
   def show
     render json: User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update!(user_params)
+    render json: { result: 'ok' }
+  end
+
+  def user_params
+    params.permit(:name)
   end
 end
